fc57a770a9f12bc057e511a6b6ac5f76360f89bd
Raunak kumar <raunakkumar7474@gmail.com>
2025-06-16 15:02:38 +0530

    Skill add

--- app/Components/resume/SkillsData.js
+++ app/Components/resume/SkillsData.js
@@ -14,71 +14,66 @@ const SkillsData = [
   {
     id: 3,
     category: "skills",
-    title: "HTML5",
-    imgSrc: "https://i.ibb.co/GF3G5Tv/html5.png",
+    title: "JAVA",
+    imgSrc: "https://i.ibb.co/VYmCNNg3/Download.png",
   },
   {
     id: 4,
     category: "skills",
-    title: "CSS3",
-    imgSrc: "https://i.ibb.co/0VwdkQn/css3.png",
+    title: "HTML",
+    imgSrc: "https://i.ibb.co/GF3G5Tv/html5.png",
   },
   {
     id: 5,
+    category: "skills",
+    title: "CSS",
+    imgSrc: "https://i.ibb.co/0VwdkQn/css3.png",
+  },
+  {
+    id: 6,
     category: "skills",
     title: "JavaScript",
     imgSrc: "https://i.ibb.co/mN8D2fr/javascript.png",
   },
   {
-    id: 6,
+    id: 7,
     category: "skills",
     title: "React",
     imgSrc: "https://i.ibb.co/3BYhD11/react.png",
   },
   {
-    id: 7,
-    category: "skills",
-    title: "TypeScript",
-    imgSrc: "https://i.ibb.co/7Nb8RpJ/typescript.png",
-  },
-  {
     id: 8,
     category: "skills",
-    title: "Bootstrap",
-    imgSrc: "https://i.ibb.co/VBWbn4Z/bootstrap.png",
+    title: "SQL",
+    imgSrc: "https://i.ibb.co/7tsCj8SK/sql-png.jpg",
   },
   {
     id: 9,
-    category: "skills",
-    title: "Angular",
-    imgSrc: "https://i.ibb.co/3z5nVf8/angular.png",
-  },
-  {
-    id: 10,
     category: "tools",
     title: "Git",
     imgSrc: "https://i.ibb.co/TbgRjCf/git.png",
   },
   {
-    id: 11,
+    id: 10,
     category: "tools",
     title: "Npm",
     imgSrc: "https://i.ibb.co/7gTcFz9/npm.png",
   },
   {
-    id: 12,
+    id: 11,
     category: "tools",
     title: "Cmd Line",
     imgSrc: "https://i.ibb.co/VY78tTB/command.png",
   },
   {
-    id: 13,
+    id: 12,
     category: "tools",
     title: "VS Code",
     imgSrc: "https://i.ibb.co/5KxhcfM/vs-code.png",
   },
+
 ];
 
 export default SkillsData;
 
-  
+
