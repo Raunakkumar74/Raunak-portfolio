f1fe8c69e3fc3e0a516d40fc35e14ea9e5bf86a0
Raunak kumar <raunakkumar7474@gmail.com>
2025-06-16 15:21:39 +0530

    Add two prpject

--- app/Components/portfolio/Menu.js
+++ app/Components/portfolio/Menu.js
@@ -1,45 +1,59 @@
 
-const Menu =[
-    {
-      id:1,
-      Image: "https://i.ibb.co/qk5ZXfZ/Screenshot-2024-08-06-003624.png",
-      title:"Animal Slider Website",
-      category: "Design",
-      link:"https://github.com/Raunakkumar74/All-Websites-Designs"
-    },
-    {
-      id:2,
-      Image: "https://i.ibb.co/qkMcdNT/Screenshot-2024-08-06-003545.png",
-      title:"Travel Website",
-      category: "Design",
-      link:"https://github.com/Raunakkumar74/All-Websites-Designs"
-    },
-    {
-      id:3,
-      Image: "https://i.ibb.co/Wk2KTqs/RickShaw.jpg",
-      title:"RickShaw APP Design",
-      category: "Design"
-    },
-    {
-      id:4,
-      Image: "https://i.ibb.co/ryYm8XT/Screenshot-2024-08-06-002608.png",
-      title:"Github Search zone",
-      category: "Applications",
-      link:"https://github.com/Raunakkumar74/GitHubSearchZone"
-    },
-    {
-      id:5,
-      Image: "https://i.ibb.co/d4G7YYx/Screenshot-2024-08-06-002302.png",
-      title:"Translate app",
-      category: "Applications",
-      link:"https://github.com/Raunakkumar74/Translate-app"
-    },
-    {
-      id:6,
-      Image: "https://i.ibb.co/6mP0szw/Screenshot-2024-08-06-002232.png",
-      title:"Calculator ",
-      category: "Applications",
-      link:"https://github.com/Raunakkumar74/Calculator"
-    },
-  ]
-  export default Menu
+const Menu = [
+  {
+    id: 1,
+    Image: "https://i.ibb.co/qk5ZXfZ/Screenshot-2024-08-06-003624.png",
+    title: "Animal Slider Website",
+    category: "Design",
+    link: "https://github.com/Raunakkumar74/All-Websites-Designs"
+  },
+  {
+    id: 2,
+    Image: "https://i.ibb.co/qkMcdNT/Screenshot-2024-08-06-003545.png",
+    title: "Travel Website",
+    category: "Design",
+    link: "https://github.com/Raunakkumar74/All-Websites-Designs"
+  },
+  {
+    id: 3,
+    Image: "https://i.ibb.co/Wk2KTqs/RickShaw.jpg",
+    title: "RickShaw APP Design",
+    category: "Design"
+  },
+  {
+    id: 4,
+    Image: "https://i.ibb.co/SwrdDXZB/1747994238236-screenshot.jpg",
+    title: "Fliescoket",
+    category: "Applications",
+    link: "https://github.com/Raunakkumar74/FileSocket"
+  },
+  {
+    id: 5,
+    Image: "https://i.ibb.co/XfymnMMw/Screenshot-2025-06-16-151738.png",
+    title: "Audio to text convetor",
+    category: "Applications",
+    link: "https://github.com/Raunakkumar74/Audio-To-TextConvert"
+  },
+  {
+    id: 6,
+    Image: "https://i.ibb.co/ryYm8XT/Screenshot-2024-08-06-002608.png",
+    title: "Github Search zone",
+    category: "Applications",
+    link: "https://github.com/Raunakkumar74/GitHubSearchZone"
+  },
+  {
+    id: 7,
+    Image: "https://i.ibb.co/d4G7YYx/Screenshot-2024-08-06-002302.png",
+    title: "Translate app",
+    category: "Applications",
+    link: "https://github.com/Raunakkumar74/Translate-app"
+  },
+  {
+    id: 8,
+    Image: "https://i.ibb.co/6mP0szw/Screenshot-2024-08-06-002232.png",
+    title: "Calculator ",
+    category: "Applications",
+    link: "https://github.com/Raunakkumar74/Calculator"
+  },
+]
+export default Menu
